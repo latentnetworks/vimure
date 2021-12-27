1333b62a6a64be0ede2388d51f723cf3230f716f
Jonathan Cardoso-Silva <jonathan.car.silva@gmail.com>
2021-12-27 19:22:47 +0000

    🔧 Update requirements to work with any local Python installation

--- src/python/setup.py
+++ src/python/setup.py
@@ -15,7 +15,7 @@ setup(
         "scipy >= 0.19.1",
         "pandas >= 0.20.3",
         "scikit-learn >= 0.19.0",
-        "scikit-tensor-py3 @ https://github.com/evertrol/scikit-tensor-py3/archive/99190292b9564e7c4a9312280db732ceedf7b7e3.zip",
+        "scikit-tensor-py3 @ https://github.com/jonjoncardoso/scikit-tensor-py3/archive/master.zip",
         "networkx >= 2.0.0",
         "plotnine >= 0.7.0"
     ],
