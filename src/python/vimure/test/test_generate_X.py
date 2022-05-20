333b3aaa9bfdeabe2df6d040e5ed69b9915ed3e3
Jonathan Cardoso-Silva <jonathan.car.silva@gmail.com>
2022-05-20 13:07:28 +0100

    🧪 Add tests test_build_X_baselines_exist + test_build_X_ensure_baselines_are_binary

--- src/python/vimure/test/test_generate_X.py
+++ src/python/vimure/test/test_generate_X.py
@@ -1,6 +1,7 @@
 import pytest
 
 import numpy as np
+import pandas as pd
 import vimure as vm
 import networkx as nx
 import sktensor as skt
@@ -51,7 +52,7 @@ def check_objects(gt_network):
 
 
 class TestGenerateXForStandardSBM:
-    def test_generate_X_binarize_False(self):
+    def test_build_X_param_cutoff_X_False(self):
 
         gt_network = vm.synthetic.StandardSBM(
             N=20,
@@ -83,7 +84,7 @@ class TestGenerateXForStandardSBM:
                     gt_network.X.toarray()[l, :, :m],
                 )
 
-    def test_generate_X_binarize_True(self):
+    def test_build_X_param_cutoff_X_True(self):
 
         gt_network = vm.synthetic.StandardSBM(
             N=20, M=20, L=1, K=4, C=2, avg_degree=4, sparsify=False, seed=10
@@ -103,7 +104,7 @@ class TestGenerateXForStandardSBM:
                     gt_network.X.toarray()[l, :, :m],
                 )
 
-    def test_generate_X_different_seeds(self):
+    def test_build_X_with_different_seeds(self):
         """
         Tests whether running the function without informing any particular seed
             indeed leads to different tensors, guaranteeing that our code is not reusing seed parameters.
@@ -126,3 +127,149 @@ class TestGenerateXForStandardSBM:
             network1.X.toarray(),
             network2.X.toarray(),
         )
+
+    def test_build_X_baselines_exist(self):
+        """
+        Asserts that Union and Intersection baselines are produced and are correct after build_X is invoked
+        """
+
+        gt_network = vm.synthetic.StandardSBM(
+            N=20,
+            M=20,
+            L=1,
+            K=2,
+            C=2,
+            avg_degree=4,
+            sparsify=False,
+            seed=10,
+        )
+
+        gt_network.build_X(
+            flag_self_reporter=True,
+            cutoff_X=True,
+            mutuality=0.5,
+            seed=20,
+        )
+
+        X = gt_network.X.toarray()
+
+        """
+        CHECK X_union
+        """
+        
+        assert gt_network.X_union is not None, "X_union is None!"
+        assert isinstance(gt_network.X_union, skt.sptensor) or isinstance(gt_network.X_union, skt.dtensor)
+        assert gt_network.X_union.shape == (
+            gt_network.L,
+            gt_network.N,
+            gt_network.N
+        )
+
+        # Calculate the expected X_union
+
+        sumX = np.sum(X, axis=3)
+        expected_X_union = np.zeros(sumX.shape).astype('int')
+        expected_X_union[sumX > 0] = 1
+
+        np.testing.assert_array_equal(gt_network.X_union.toarray(), 
+                                      expected_X_union, 
+                                      err_msg="X_union produced by the algorithm is not as expected")
+
+        """
+        CHECK X_intersection
+        """
+        assert gt_network.X_intersection is not None, "X_intersection is None!"
+        assert (isinstance(gt_network.X_intersection, skt.sptensor) or 
+                isinstance(gt_network.X_intersection, skt.dtensor))
+
+        assert gt_network.X_intersection.shape == (
+            gt_network.L,
+            gt_network.N,
+            gt_network.N,
+        )
+
+        expected_X_intersection = np.zeros(sumX.shape).astype('int')
+
+        # Because flag_self_reporter=True, the maximum number of reporters that can report on a specific tie is 2. 
+        # This is because, under this assumption, reporters are only asked about ties they are ego or alter.
+        expected_X_intersection[sumX == gt_network.K] = 1
+
+        np.testing.assert_array_equal(gt_network.X_intersection.toarray(), 
+                                      expected_X_intersection, 
+                                      err_msg="X_intersection produced by the algorithm is not as expected")
+
+    def test_build_X_ensure_baselines_are_binary(self):
+        """
+        Asserts that Union and Intersection baselines are produced and correct even when K is higher than 2
+
+        K represents the maximum entry value in the ground truth Y and it is also used by build_X to set Q 
+        (if the user does not provide a value for Q).
+        With this test, we want to ensure that the baselines are still binary regardless of K.
+
+        """
+
+        gt_network = vm.synthetic.StandardSBM(
+            N=20,
+            M=20,
+            L=1,
+            K=5, ## This is what makes this test different to the previous one
+            C=2,
+            avg_degree=4,
+            sparsify=False,
+            seed=10,
+        )
+
+        gt_network.build_X(
+            flag_self_reporter=True,
+            cutoff_X=True,
+            mutuality=0.5,
+            seed=20,
+        )
+
+        X = gt_network.X.toarray()
+
+        """
+        CHECK X_union
+        """
+
+        assert gt_network.X_union is not None, "X_union is None!"
+        assert isinstance(gt_network.X_union, skt.sptensor) or isinstance(gt_network.X_union, skt.dtensor)
+        assert gt_network.X_union.shape == (
+            gt_network.L,
+            gt_network.N,
+            gt_network.N
+        )
+
+        # Calculate the expected X_union
+
+        sumX = np.sum(X, axis=3)
+        expected_X_union = np.zeros(sumX.shape).astype('int')
+        expected_X_union[sumX > 0] = 1
+
+        np.testing.assert_array_equal(gt_network.X_union.toarray(), 
+                                        expected_X_union, 
+                                        err_msg="X_union produced by the algorithm is not as expected")
+
+        """
+        CHECK X_intersection
+        """
+        assert gt_network.X_intersection is not None, "X_intersection is None!"
+        assert (isinstance(gt_network.X_intersection, skt.sptensor) or 
+                isinstance(gt_network.X_intersection, skt.dtensor))
+
+        assert gt_network.X_intersection.shape == (gt_network.L, gt_network.N, gt_network.N)
+
+        nonzero_lijm = pd.DataFrame(np.stack(gt_network.X.subs).T, columns=["l", "i", "j", "m"])
+
+        # Because flag_self_reporter=True, the maximum number of reporters that can report on a specific tie is 2. 
+        # This is because, under this assumption, reporters are only asked about ties they are ego or alter.
+        nonzero_indices = nonzero_lijm.groupby(["l", "i", "j"]).count().query("m == 2")
+        nonzero_indices = nonzero_indices.reset_index().drop(columns="m")
+
+        expected_X_intersection = np.zeros(sumX.shape).astype('int')
+        expected_X_intersection[nonzero_indices["l"], nonzero_indices["i"], nonzero_indices["j"]] = 1
+
+        np.testing.assert_array_equal(gt_network.X_intersection.toarray(), 
+                                        expected_X_intersection, 
+                                        err_msg="X_intersection produced by the algorithm is not as expected")
+
