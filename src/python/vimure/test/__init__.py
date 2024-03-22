effdc3c0c0784ff511ceded42fa51e02bc83e52e
Jon Cardoso-Silva <jonathan.car.silva@gmail.com>
2024-03-22 11:21:59 +0000

    🧪  Make path to Karnataka data relative so that VSCode testing suite works

--- src/python/vimure/test/__init__.py
+++ src/python/vimure/test/__init__.py
@@ -1,8 +1,18 @@
 """Unittests"""
+import os
+import sys
 import pytest
 
 from os import devnull
 from contextlib import contextmanager,redirect_stderr,redirect_stdout
+
+### To guarantee that the experiments folder is in the path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+experiments_dir = os.path.join(current_dir, '..', '..', '..', '..', 'notebooks', 'python', 'experiments')
+sys.path.insert(0, experiments_dir)
+from karnataka import read_village_data # type: ignore
+
+KARNATA_DATA_FOLDER = os.path.join(current_dir, "..", "..", "..", "..", "data/input/india_microfinance/formatted/")
 
 @contextmanager
 def suppress_stdout_stderr():
@@ -17,26 +27,18 @@ def suppress_stdout_stderr():
 
 @pytest.fixture()
 def karnataka_edgelist_vil1():
-
-    import sys
-    sys.path.insert(0, "notebooks/python/experiments/")
-
-    from karnataka import read_village_data # type: ignore
+    
     df, nodes, reporters = read_village_data("vil1", 
-                                             data_folder="data/input/india_microfinance/formatted/")
+                                             data_folder=KARNATA_DATA_FOLDER)
     df.rename(columns={"Ego": "ego", "Alter": "alter"}, inplace=True)
     yield df, nodes, reporters
 
 @pytest.fixture()
 def karnataka_edgelist_vil1_money():
 
-    import sys
-    sys.path.insert(0, "notebooks/python/experiments/")
-
-    from karnataka import read_village_data # type: ignore
     df, nodes, reporters = read_village_data("vil1", 
                                              filter_layer="money",
-                                             data_folder="data/input/india_microfinance/formatted/")
+                                             data_folder=KARNATA_DATA_FOLDER)
     df.rename(columns={"Ego": "ego", "Alter": "alter"}, inplace=True)
     yield df, nodes, reporters
 
